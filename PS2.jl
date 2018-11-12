40977a74b81a0507efc41e4cdb7087c5541d8367
CA <aguil157@umn.edu>
2018-11-12 12:00:19 -0600

    C2

--- PS2.jl
+++ PS2.jl
@@ -1,25 +1,24 @@
-# Trying 
+# Trying
 
 using Optim
 using LinearAlgebra
 using Plots
 using ForwardDiff
-using NLSolversBase
 using NLsolve
 
 # 1) Write a code that derives the log-linearized solution to the prototype
 #growth model with wedges in state-space form with measurement error
 # First, we need to find the steady state of the model
 
-θ, ψ, τxss, τlss, β̄, δ, zss, gn, gz, gss = .33, 2.5, .2, .3, .975, .05, 1, 1.01, 1.019, .5
+θ, ψ, τxss, τlss, β̄, δ, zss, gn, gz, gss, σ = .33, 2.5, .2, .3, .975, .05, 1, 1.01, 1.019, .5, 1
 
 β = β̄*gn
 
 function v!(F,x)
     # x[1]=k , x[2]= c , x[3]=l
-    F[1] = x[1] - (((1+τx)*(1-β*(1-δ))/(β*θ*(zss^(1-θ))))^(1/(θ-1))) *x[3]
+    F[1] = x[1] - (((1+τxss)*(1-β*(1-δ))/(β*θ*(zss^(1-θ))))^(1/(θ-1))) *x[3]
     F[2] = x[2] - ((x[1]/x[3])^(θ-1) * (zss^(1-θ)) - (gz)*(gn) + (1-δ))*x[1]+gss
-    F[3] = x[2] - ((1-τl)*(1-θ)*(x[1]/x[3])^θ * zss^(1-θ)/ψ)*(1-x[3])
+    F[3] = x[2] - ((1-τlss)*(1-θ)*(x[1]/x[3])^θ * zss^(1-θ)/ψ)*(1-x[3])
 end
 
 intial_x = [4, .56 , .3]
@@ -66,8 +65,47 @@ grad_L= ForwardDiff.gradient(Labor,[0,0,0,0,0,0])
 # Since we have an implicit function of L, we need to divide all the coefficients
 # by the coefficient for L and mulltiply by -1 to get L as a function of the states:
 coeff_L = -grad_L/ grad_L[4]
+# Eliminating the L coefficient (= -1)
+Φl =filter(e->e≠coeff_L[4],coeff_L)
+
+
+
 # Just to check if I did not do something wrong, the coefficient (in negative) for L (page 8):
 a=-((ψ*(((kss^θ)*(((zss)*(lss))^(1-θ)))*(1-θ))+((((kss*exp(0))^θ)*(((zss*exp(0))*(lss*exp(0)))^(1-θ))))*(1-θ)*(1-τlss))+a2*(1-τlss)*θ)
 # Checking the coefficient for g :
 ϕ_g = -ψ*gss/a
 # Which is very close
+
+# Now I will proceed to estimate Output
+
+# Output
+
+yss = kss^(θ)*(zss*lss)^(1-θ)
+function Y(x)
+    k,z,l = x
+    m = log((kss^(θ)*exp(θ*k)*(zss*lss)^(1-θ)*exp((1-θ)*z)*exp((1-θ)*l))/yss)
+end
+
+Y([0,0,0])
+coeff_Y = ForwardDiff.gradient(Y, [0,0,0])
+
+# Getting the coefficients for L as a function of  k1, k2, z, g, τ
+Φ_yl = coeff_Y[3]*Φl
+Φ_yk1 = coeff_Y[1]+ Φ_yl[1]
+Φ_yk2 = Φ_yl[2]
+Φ_yz = Φ_yl[3] + coeff_Y[2]
+Φ_yg = Φ_yl[4]
+Φ_yτ = Φ_yl[5]
+
+# For Investment, I use law motion of capital:
+xss = kss*(gn*gz) - ((1-δ)*kss)
+function Inv(x)
+    k1, k2 = x
+    m = log(((kss*exp(k2))*(gn*gz) - ((1-δ)*kss*exp(k1)))/xss)
+end
+
+Inv([0,0])
+
+coeff_I = ForwardDiff.gradient(Inv,[0,0])
+
+# Now I proceed with the Euler Equation
